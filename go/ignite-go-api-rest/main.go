a91f1c13faf5b7b3a8156d170233b0acf4c680ab
Kemuel Batista <kemuellima20@gmail.com>
2025-07-03 11:19:39 -0300

    feature: go: post requests and error handling

--- go/ignite-go-api-rest/main.go
+++ go/ignite-go-api-rest/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -36,7 +39,7 @@ func main() {
 		r.Use(jsonMiddleware)
 
 		r.Get("/users/{id:[0-9]+}", handleGetUsers(db))
-		r.Post("/users", handlePostUsers)
+		r.Post("/users", handlePostUsers(db))
 	})
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
@@ -61,17 +64,55 @@ func handleGetUsers(db map[int64]User) http.HandlerFunc {
 		// Pois o usuário pode passar um valor enorme e dar erro de overflow do 64bit
 		id, _ := strconv.ParseInt(idStr, 10, 64)
 
+		// Isso é um bug, não funciona como esperado
+		// Toda vez que recebemos uma request em GO, automaticamente o pacote go cria uma go-routine para tratar
+		// a request, ou seja, o código vai rodar pararelamente ou concurrentemente, isso não é um problema, pois
+		// estou fazendo requests para mim mesmo, mas aplicação real, isso pode ser um problema, pois são várias
+		// requests ao mesmo tempo
+		// Estamos escrevendo e lendo para mesma variavel, causando uma race condition, pois o mapa é um ponteiro para um mapa
 		user, ok := db[id]
-		if ok {
-			data, err := json.Marshal(user)
-
-			if err != nil {
-				panic(err)
-			}
-
-			w.Write(data)
+		if !ok {
+			// http.Error(w, "User not found", http.StatusNotFound) // Returns plain/text not a JSON
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(`{"error": "User not found"}`)) // Returning a JSON error message
+			return
 		}
+
+		data, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(data)
 	}
 }
 
-func handlePostUsers(w http.ResponseWriter, r *http.Request) {}
+func handlePostUsers(db map[int64]User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.Body = http.MaxBytesReader(w, r.Body, 10000) // Limiting the request body to 10MB
+		data, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			var maxErr *http.MaxBytesError
+			if errors.As(err, &maxErr) {
+				http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
+				return
+			}
+
+			fmt.Println(err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		var user User
+		if err := json.Unmarshal(data, &user); err != nil {
+			http.Error(w, "Invalid body", http.StatusUnprocessableEntity)
+		}
+
+		// this line is a bug, not works as expected
+		db[user.ID] = user
+
+		w.WriteHeader(http.StatusCreated)
+	}
+}
