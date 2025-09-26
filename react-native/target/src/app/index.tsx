44fe53aad3982df8be3fcc58d31c51ce3d5dcced
Kemuel Batista <kemuellima20@gmail.com>
2025-09-26 17:19:52 -0300

    feature: home header component implementation

--- react-native/target/src/app/index.tsx
+++ react-native/target/src/app/index.tsx
@@ -1,23 +1,11 @@
-import { router } from 'expo-router'
-import { Button, Text, View } from 'react-native'
+import { View } from 'react-native'
 
-import { fontFamily } from '@/theme/font-family'
+import { HomeHeader } from '@/components/home-header'
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold }}>Olá, expo router</Text>
-
-      <Button title="Nova meta" onPress={() => router.navigate('/target')} />
-      <Button
-        title="Transação"
-        onPress={() => router.navigate('/transaction/132')}
-      />
-
-      <Button
-        title="Progress"
-        onPress={() => router.navigate('/in-progress/132')}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   )
 }
