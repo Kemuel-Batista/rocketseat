6ed986e9737d1b40e59c0e949ba0f5c3aa8b4774
Kemuel Batista <kemuellima20@gmail.com>
2025-09-29 14:26:29 -0300

    feature: target component

--- react-native/target/src/app/index.tsx
+++ react-native/target/src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
 
 import { HomeHeader } from '@/components/home-header'
+import { Target } from '@/components/target'
 
 const summary = {
   total: 'R$ 2,680,00',
@@ -8,10 +9,21 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883.65' },
 }
 
+const targets = [
+  {
+    name: 'Comprar Teclado Logitech',
+    percentage: '75%',
+    current: 'R$ 900,00',
+    target: 'R$ 1.000,00',
+  },
+]
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <Target data={targets[0]} />
     </View>
   )
 }
