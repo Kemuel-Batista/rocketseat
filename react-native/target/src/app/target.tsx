13e55d62b2cdfb0755075b527af46298d937e216
Kemuel Batista <kemuellima20@gmail.com>
2025-10-01 09:14:11 -0300

    feature: implement target creation functionality with database integration

--- react-native/target/src/app/target.tsx
+++ react-native/target/src/app/target.tsx
@@ -1,11 +1,58 @@
-import { View } from 'react-native'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { Alert, View } from 'react-native'
 
 import { Button } from '@/components/button'
 import { CurrencyInput } from '@/components/currency-input'
 import { Input } from '@/components/input'
 import { PageHeader } from '@/components/page-header'
+import { useTargetDatabase } from '@/database/use-target-database'
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const params = useLocalSearchParams<{ id?: string }>()
+  const targetDatabase = useTargetDatabase()
+
+  async function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert(
+        'Atenção',
+        'Preencha nome e o valor precisa ser maior que zero.',
+      )
+    }
+
+    setIsProcessing(true)
+
+    if (params.id) {
+      // update
+    } else {
+      await create()
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({
+        name,
+        amount,
+      })
+
+      Alert.alert('Nova meta', 'Meta criada com sucesso!', [
+        {
+          text: 'OK',
+          onPress: () => router.back(),
+        },
+      ])
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível criar a meta.')
+      console.log(error)
+      setIsProcessing(false)
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -15,13 +62,23 @@ export default function Target() {
 
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input
+          value={name}
+          onChangeText={setName}
           label="Nome da marca"
           placeholder="Ex: Viagem para praia, Apple Watch"
         />
 
-        <CurrencyInput label="Valor alvo (R$)" value={0} />
+        <CurrencyInput
+          label="Valor alvo (R$)"
+          value={amount}
+          onChangeValue={setAmount}
+        />
 
-        <Button title="Salvar" />
+        <Button
+          title="Salvar"
+          onPress={handleSave}
+          isProcessing={isProcessing}
+        />
       </View>
     </View>
   )
