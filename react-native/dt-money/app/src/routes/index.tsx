52aca9cc954cafc14108a75ed502a7e54793da92
Kemuel Batista <kemuellima20@gmail.com>
2025-10-08 17:37:48 -0300

    feature: dismiss keyboard view implementation

--- react-native/dt-money/app/src/routes/index.tsx
+++ react-native/dt-money/app/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { useCallback, useState } from 'react'
+import { SystemBars } from 'react-native-edge-to-edge'
 
 import { PrivateRoutes } from './private-routes'
 import { PublicRoutes } from './public-routes'
@@ -19,6 +20,7 @@ export function NavigationRoutes() {
 
   return (
     <NavigationContainer>
+      <SystemBars style="light" />
       <Routes />
     </NavigationContainer>
   )
--- react-native/dt-money/app/src/screens/login/index.tsx
+++ react-native/dt-money/app/src/screens/login/index.tsx
@@ -1,18 +1,19 @@
 import { useNavigation } from '@react-navigation/native'
 import type { StackNavigationProp } from '@react-navigation/stack'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 
-import type { PublicStackParamsList } from '@/routes'
+import { DismissKeyboardView } from '@/components/dismiss-keyboard-view'
+import type { PublicStackParamsList } from '@/routes/public-routes'
 
 export function Login() {
   const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>()
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <DismissKeyboardView>
       <Text>Tela de login!</Text>
       <TouchableOpacity onPress={() => navigation.navigate('register')}>
         <Text>Regitrar</Text>
       </TouchableOpacity>
-    </View>
+    </DismissKeyboardView>
   )
 }
