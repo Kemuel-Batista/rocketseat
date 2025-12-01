33416176823749e84a687fe823e616585ac1b46c
Kemuel Batista <kemuellima20@gmail.com>
2025-12-01 15:35:47 -0300

    feature: create users implementation

--- go/ignite-go-advanced-http-servers/gobid/cmd/api/main.go
+++ go/ignite-go-advanced-http-servers/gobid/cmd/api/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"gobid/internal/api"
+	"gobid/internal/services"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, GoBid API!")
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	api := api.Api{
+		Router:      chi.NewMux(),
+		UserService: services.NewUserService(pool),
+	}
+
+	api.BindRoutes()
+
+	fmt.Println("Starting server on :3080")
+	if err := http.ListenAndServe("localhost:3080", api.Router); err != nil {
+		panic(err)
+	}
 }
--- go/ignite-go-advanced-http-servers/gobid/internal/api/user-handlers.go
+++ go/ignite-go-advanced-http-servers/gobid/internal/api/user-handlers.go
@@ -1,9 +1,40 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"gobid/internal/jsonutils"
+	"gobid/internal/services"
+	"gobid/internal/usecase/user"
+	"net/http"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	panic("TODO")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserUseCaseRequest](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(
+		r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+		data.Bio,
+	)
+
+	if err != nil {
+		errors.Is(err, services.ErrDuplicatedEmailOrPassword)
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+			"error": "email or username already exists",
+		})
+		return
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
 }
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
