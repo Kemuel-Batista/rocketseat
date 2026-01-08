362732a11c7e9d879273aa1a2278e1f3903ba22b
Kemuel Batista <kemuellima20@gmail.com>
2026-01-08 16:51:31 -0300

    chore: nativewind implementation

--- react-native/mvvm-architecture/marketplace-app/src/app/index.tsx
+++ react-native/mvvm-architecture/marketplace-app/src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text className="text-red-100">Hello world</Text>
     </View>
   )
 }
