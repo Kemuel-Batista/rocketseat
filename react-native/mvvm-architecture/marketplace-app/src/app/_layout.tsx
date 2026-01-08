ab9d4db6339233b42dbd5c49ec2f1176c463e374
Kemuel Batista <kemuellima20@gmail.com>
2026-01-08 16:55:34 -0300

    chore: react query configuration

--- react-native/mvvm-architecture/marketplace-app/src/app/_layout.tsx
+++ react-native/mvvm-architecture/marketplace-app/src/app/_layout.tsx
@@ -1,7 +1,18 @@
 import '../styles/global.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { Stack } from 'expo-router'
 
+const queryClient = new QueryClient()
+
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </QueryClientProvider>
+  )
 }
