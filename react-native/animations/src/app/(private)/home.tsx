c2b737d3767347e2028e433bbdbad827337b020a
Kemuel Batista <kemuellima20@gmail.com>
2026-08-28 23:10:26 -0300

    feature: finishing login screen

--- react-native/animations/src/app/(private)/home.tsx
+++ react-native/animations/src/app/(private)/home.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from 'react-native'
+import { useAuthStore } from '@/shared/stores/auth.store'
+import { router } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export default function Home() {
+  const { logout } = useAuthStore()
+
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Text>Home</Text>
+      <TouchableOpacity
+        onPress={() => {
+          logout()
+          router.replace('/(public)/login')
+        }}
+      >
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
