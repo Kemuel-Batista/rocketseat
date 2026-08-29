9b185346c6c9d6e11a07bac5d6cadb323dd8fb47
Kemuel Batista <kemuellima20@gmail.com>
2026-08-29 13:43:10 -0300

    feature: home header implementation

--- react-native/animations/src/screens/home/home.view.tsx
+++ react-native/animations/src/screens/home/home.view.tsx
@@ -1,4 +1,5 @@
 import { AppText } from '@/components/app-text'
+import { HomeHeader } from '@/components/home-header'
 import { colors } from '@/constants/colors'
 import { StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -7,6 +8,7 @@ export function HomeView() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
+        <HomeHeader />
         <AppText>Home</AppText>
       </View>
     </SafeAreaView>
